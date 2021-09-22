c97cbcf7946cf3c5ca163ca1f7d136e7f290a49f
pongotamaas <90310334+pongotamaas@users.noreply.github.com>
2021-09-22 09:20:39 +0200

    melegszedvicccss

---
+++ pongo.py
@@ -0,0 +1,12 @@
+# recept_03.py
+
+def Melegszendvics(cheese_type, side_dish):
+    print('Kend meg a kenyeret.')
+    print('Válaszd ki a feltétet.')
+    print('Válaszd kia a szajtot.') 
+    print('Ha kiválasztotad akkor pakold a kenyérre.')
+    print('És ted a sűtöbe 5-10 percig.')
+    print('Szórj rá reszelt ' + cheese_type + '-t.')
+    print('Szolgáld fel ' + side_dish + '-val/vel.')
+
+Melegszendvics('edami,', 'parizel')
